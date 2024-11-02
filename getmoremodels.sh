7517be01456099313fcb3e581923deef3edc5599
kycg <95092804+kycg@users.noreply.github.com>
2024-11-02 20:28:34 +0000

    Update getmoremodels.sh

--- getmoremodels.sh
+++ getmoremodels.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-#Location  wget https://raw.githubusercontent.com/kycg/ai-dock-deafult/refs/heads/main/getmoremodels.sh
-#2 		   chmod +x getmoremodels.sh
-#3		   ./getmoremodels.sh
+# Location  wget https://raw.githubusercontent.com/kycg/ai-dock-deafult/refs/heads/main/getmoremodels.sh
+# 2         chmod +x getmoremodels.sh
+# 3         ./getmoremodels.sh
 
 download_model() {
     local url=$1
     local destination_path=$2
 
-
     mkdir -p "$(dirname "$destination_path")"
-
 
     echo "Downloading $destination_path ..."
     wget -O "$destination_path" "$url" --tries=3 --continue
 
-
     if [ $? -eq 0 ]; then
         echo "ok $destination_path"
     else
-        echo "Error：$url"
+        echo "Error: $url"
     fi
 }
 
 download_model "https://huggingface.co/alimama-creative/FLUX.1-dev-Controlnet-Inpainting-Beta/resolve/main/diffusion_pytorch_model.safetensors?download=true" \
                "/workspace/Comfyui/models/controlnet/cn-inpaint-control.safetensors"
-
