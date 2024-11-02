4330e9b34edff87dd296607ed4b852d6174f9d6a
kycg <95092804+kycg@users.noreply.github.com>
2024-11-02 22:59:46 +0000

    Update getmoremodels.sh

--- getmoremodels.sh
+++ getmoremodels.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Location  wget https://raw.githubusercontent.com/kycg/ai-dock-deafult/refs/heads/main/getmoremodels.sh
+# 1.1		dos2unix getmoremodels.sh
 # 2         chmod +x getmoremodels.sh
 # 3         ./getmoremodels.sh
+
+# 99  tar -cvf output.tar /workspace/ComfyUI/output
 
 download_model() {
     local url=$1
@@ -20,5 +23,16 @@ download_model() {
     fi
 }
 
+download_model "https://huggingface.co/Kijai/flux-fp8/resolve/main/flux1-dev-fp8-e4m3fn.safetensors?download=true" \
+               "/workspace/ComfyUI/models/unet/flux1-dev-fp8-e4m3fn.safetensors"
+			   
 download_model "https://huggingface.co/alimama-creative/FLUX.1-dev-Controlnet-Inpainting-Beta/resolve/main/diffusion_pytorch_model.safetensors?download=true" \
-               "/workspace/Comfyui/models/controlnet/cn-inpaint-control.safetensors"
+               "/workspace/ComfyUI/models/controlnet/cn-inpaint-control.safetensors"
+
+#download_model "https://huggingface.co/openai/clip-vit-large-patch14/resolve/main/model.safetensors" \
+               "/workspace/ComfyUI/models/clip/clip-vit-large-patch14.safetensors"
+			   
+
+#download_model  "https://huggingface.co/lodestones/stable-diffusion-3-medium/resolve/4a708bd3d18c10253247f8660cd4ffae6cd63bf1/stable-diffusion-3-medium/text_encoders/clip_g.safetensors" \
+#               "/workspace/ComfyUI/models/clip/clip_g.safetensors"
+			   
